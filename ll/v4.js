3b08632cf0ca6c36afaabd345a7ec23192b91f44
whhuber <57718898+whhuber@users.noreply.github.com>
2019-12-10 14:44:17 +0100

    Create v4.js

---
+++ ll/v4.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+//import './App.css';
+
+class App extends Component {
+	constructor( props ) {
+		super();
+	}
+	render() {
+		return (
+			<>
+				<Scenery></Scenery>
+				<Lander></Lander>
+			</>
+		)
+	}
+}
+
+class Lander extends Component {
+	constructor( props ) {
+		super( props );
+		this.state = { hei: 0, spe: 0 };
+	}
+	render() {	
+		let sty = {
+			backgroundColor:	'gold',			
+			position:					'absolute',
+			top:							this.state.hei + 'vh',
+			left:							'48vw',
+			width:						'4vw',
+			height:						'4vh',
+		}
+		return (
+			<>
+				<div style={sty}>{this.state.hei}</div>
+			</>
+		)
+	}
+}
+
+class Scenery extends Component {
+	render() {
+		return ( 
+			<>
+				<div style={{backgroundColor: 'black', position: 'absolute', 
+					top: '0', left: '0', width: '100vw', height: '90vh'}}></div>
+				<div style={{backgroundColor: 'silver', position: 'absolute', 
+					top: '90vh', left: '0', width: '100vw', height: '10vh'}}></div>
+			</>
+		)
+	}
+}
+
+export default App;

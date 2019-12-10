2e1b26416428f3b5d552063a0e90af23cd7279fe
whhuber <57718898+whhuber@users.noreply.github.com>
2019-12-10 14:43:21 +0100

    Create v2.js

---
+++ ll/v2.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+//import './App.css';
+
+class App extends Component {
+	constructor( props ) {
+		super();
+	}
+	render() {
+		return (
+			<>
+				<Lander></Lander>
+			</>
+		)
+	}
+}
+
+class Lander extends Component {
+	constructor( props ) {
+		super( props );
+	}
+	render() {	
+		let sty = {
+			backgroundColor:	'gold',			
+			position:					'absolute',
+			top:							'20vh',
+			left:							'48vw',
+			width:						'4vw',
+			height:						'4vh',
+		}
+		return (
+			<>
+				<div style={sty}></div>
+			</>
+		)
+	}
+}
+
+export default App;
